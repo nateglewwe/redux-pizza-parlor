dd7cb983dc7d9dc9b44cb20f349c9b38d2c826c2
Alvin Graham <grah0071@gmail.com>
2024-02-04 20:37:44 -0600

    Establish routing in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      <Router>
+        {/*All team routes go below this line*/}
+        <Route
+          path="/"
+          exact>
+          <img src="images/pizza_photo.png" />
+          <p>Pizza is great.</p>
+        </Route>
+      </Router>
     </div>
   );
 }
