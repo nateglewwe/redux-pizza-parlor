c1c5153f98e6323652a0a6509c78ffdc035d2acb
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2024-02-05 12:50:31 -0600

    updated App.jsx with Customer route data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Customer from './Customer/Customer.jsx';
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      <Router>
+        {/*All team routes go below this line*/}
+        <Route
+          path="/"
+          exact>
+          <img src="images/pizza_photo.png" />
+          <p>Pizza is great.</p>
+        </Route>
+        <Route path="/customer">
+        <Customer />
+        </Route>
+      </Router>
     </div>
   );
 }
