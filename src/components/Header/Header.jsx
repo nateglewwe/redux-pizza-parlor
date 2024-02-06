07d1c2e9883bdedbc4e5c2e7e29da132bf67326b
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-02-06 00:27:21 -0600

    Added global state dollar amount and css to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import styles from './Header.module.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useSelector } from 'react-redux';
 
 function Header () {
+    const orderInfo = useSelector((state) => state.orderInfo);
+
     return (
     <>
-        <h1 className={styles.appTitle}>Prime Pizza</h1>
-        <p className={styles.totalCost}>Total:</p>
+        <span className={styles.appTitle}>Prime Pizza</span>
+        <span className={styles.totalCost}><ShoppingCartIcon sx={{ fontSize: 24 }}/> Total: ${orderInfo.totalPrice}</span>
     </>
     )
 }
