bee06537cebd0aad4cefdaa1ea8ae2c100c1759b
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2024-02-07 16:24:06 -0600

    added customer component

--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -1,19 +1,72 @@
+import { useDispatch } from 'react-redux';
+import { useState} from 'react';
+import './Customer.css';
 
 export default function Customer() {
 
+    const dispatch = useDispatch();
+
+    const [formData, setFormData] = useState({
+        name: '',
+        address: '',
+        city: '',
+        zip: '',
+        orderType: 'Pickup', // Default to Pickup
+    });
+
+    const handleInputChange = (e) => {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value,
+        });
+      };
+    
+      const handleOrderTypeChange = (e) => {
+        setFormData({
+          ...formData,
+          orderType: e.target.value,
+        });
+      };
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        // Dispatch an action with the customer information
+        dispatch({
+          type: 'SUBMIT_DATA',
+          payload: {
+            customerData: formData,
+          },
+        });
+        // Reset the form or navigate to the next step
+        // (depending on your application flow)
+      };
+    
+
+
+
     return (
         <>
-            <h1>Step Two: Customer Information</h1>
-        <form >
-            <input type="text" name="name" placeholder="Name"/>
-            <input type="text" name="address" placeholder="Address"/>
-            <input type="text" name="city" placeholder="City"/>
-            <input type="text" name="zip" placeholder="Zip"/>
+        <h1>Step Two: Customer Information</h1>
+        <form  onSubmit={handleSubmit}>
+            <div className="customer-form">
+            <div className="customer-data fields">
+            <input type="text" name="name" placeholder="Name" onChange={handleInputChange}/>
+            <input type="text" name="address" placeholder="Address" onChange={handleInputChange}/>
+            <input type="text" name="city" placeholder="City" onChange={handleInputChange}/>
+            <input type="text" name="zip" placeholder="Zip" onChange={handleInputChange}/>
+            <button type="submit">Test Submit Button</button>
+            </div>
             
-            <input type="radio" id="pickup" name="order_type" value="Pickup"></input>
-            {/* <label for="pickup">Pickup</label><br></br> */}
-            <input type="radio" id="delivery" name="order_type" value="Delivery"></input>
-            {/* <label for="delivery">Delivery</label><br></br> */}
+            <div className="radio-container">
+            <input type="radio" id="pickup" name="order_type" value="Pickup" checked={formData.orderType === 'Pickup'} onChange={handleOrderTypeChange}></input>
+            <label htmlFor="pickup">Pickup</label><br></br>
+            <input type="radio" id="delivery" name="order_type" value="Delivery" checked={formData.orderType === 'Delivery'} onChange={handleOrderTypeChange}></input>
+            <label htmlFor="delivery">Delivery</label>
+            </div>
+
+            
+
+            </div>
         </form>
         </>
     );
