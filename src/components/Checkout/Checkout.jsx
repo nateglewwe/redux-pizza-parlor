b323610506e43b9788b098b156c0650f6e535691
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-02-17 17:15:26 -0600

    Started Checkout .jsx and .css files

---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,29 @@
+
+
+function Checkout () {
+    return (
+    <>
+          <h1>Step Three: Checkout</h1>
+          <p>PUT CUSTOMER ADDRESS HERE</p>
+          <p>PUT PICKUP OR DELIVERY STATUS HERE, RIGHT SIDE OF DOM</p>
+          <table>
+            <tr>
+                <th>Name</th>
+                <th>Cost</th>
+            </tr>
+            <tr>
+                <td>PIZZA 1 NAME HERE</td>
+                <td>PIZZA 1 COST HERE</td>
+            </tr>
+            <tr>
+                <td>PIZZA 2 NAME HERE</td>
+                <td>PIZZA 2 COST HERE</td>
+            </tr>
+          </table>
+
+    
+    </>
+    )
+}
+
+export default Checkout;
