3b047643d7c3ed9893d74dde5f59af5e45ab4e1a
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-02-17 23:12:14 -0600

    Finished first version of Checkout component

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,33 +1,102 @@
 import './Checkout.css';
 
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 function Checkout () {
+    const customerInfo = useSelector((state) => state.customerInfo);
+    const pizzaOrderInfo = useSelector((state) => state.orderInfo);
+    const dispatch = useDispatch();
+  
+    const submitBtnClk = () => {
+      console.log("Submit Button Clicked");
+  
+      // create data package
+      const pizzaData = [];
+      pizzaOrderInfo.pizzaList.forEach((pizza) => {
+        if (pizza.quantity > 0) {
+          pizzaData.push({ id: pizza.id, quantity: pizza.quantity });
+        }
+      });
+  
+      const orderData = {
+        customer_name: customerInfo.name,
+        street_address: customerInfo.address,
+        city: customerInfo.city,
+        zip: customerInfo.zip,
+        total: pizzaOrderInfo.totalPrice,
+        type: customerInfo.orderType,
+        pizzas: pizzaData,
+      };
+  
+      axios
+        .post("/api/order", orderData)
+        .then((result) => {
+          console.log("Order submitted to server - Clearing Order data");
+          dispatch({ type: "CLEAR_PIZZA_ORDER" });
+          // dispatch({type: /* some reducer action to clear customerInfo store*/})
+        })
+        .catch((err) => {
+          console.error("ERROR in client POST:", err);
+        });
+    };
+
     return (
     <>
         <h1>Step Three: Checkout</h1>
-        <p>PUT CUSTOMER ADDRESS HERE</p>
-        <p className="DOM-right">PUT PICKUP OR DELIVERY STATUS HERE, RIGHT SIDE OF DOM</p>
+        <p className="DOM-right">{customerInfo.orderType}</p>
+        <span>{customerInfo.name}</span><br/>
+        <span>{customerInfo.address}</span><br/>
+        <span>{customerInfo.city} {customerInfo.zip}</span>
         <table>
         <thead>
             <tr>
                 <th>Name</th>
+                <th>No. of pizzas</th>
                 <th>Cost</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>PIZZA 1 NAME HERE</td>
-                <td>PIZZA 1 COST HERE</td>
+                <td>{pizzaOrderInfo.pizzaList[0].name}</td>
+                <td>{pizzaOrderInfo.pizzaList[0].quantity}</td>
+                <td>{pizzaOrderInfo.pizzaList[0].price}</td>
             </tr>
             <tr>
-                <td>PIZZA 2 NAME HERE</td>
-                <td>PIZZA 2 COST HERE</td>
+                <td>{pizzaOrderInfo.pizzaList[1].name}</td>
+                <td>{pizzaOrderInfo.pizzaList[1].quantity}</td>
+                <td>{pizzaOrderInfo.pizzaList[1].price}</td>
+            </tr>
+            <tr>
+                <td>{pizzaOrderInfo.pizzaList[2].name}</td>
+                <td>{pizzaOrderInfo.pizzaList[2].quantity}</td>
+                <td>{pizzaOrderInfo.pizzaList[2].price}</td>
+            </tr>
+            <tr>
+                <td>{pizzaOrderInfo.pizzaList[3].name}</td>
+                <td>{pizzaOrderInfo.pizzaList[3].quantity}</td>
+                <td>{pizzaOrderInfo.pizzaList[3].price}</td>
+            </tr>
+            <tr>
+                <td>{pizzaOrderInfo.pizzaList[4].name}</td>
+                <td>{pizzaOrderInfo.pizzaList[4].quantity}</td>
+                <td>{pizzaOrderInfo.pizzaList[4].price}</td>
+            </tr>
+            <tr>
+                <td>{pizzaOrderInfo.pizzaList[5].name}</td>
+                <td>{pizzaOrderInfo.pizzaList[5].quantity}</td>
+                <td>{pizzaOrderInfo.pizzaList[5].price}</td>
+            </tr>
+            <tr>
+                <td>{pizzaOrderInfo.pizzaList[6].name}</td>
+                <td>{pizzaOrderInfo.pizzaList[6].quantity}</td>
+                <td>{pizzaOrderInfo.pizzaList[6].price}</td>
             </tr>
         </tbody>
         </table>
-        <p className="DOM-right">PUT TOTAL COST OF ORDER HERE, RIGHT SIDE OF DOM</p>
+        <p className="DOM-right">{pizzaOrderInfo.totalPrice}</p>
         <br/><br/><br/>
-        <button className="DOM-right">CHECKOUT</button>
+        <button className="DOM-right" type="button" onClick={submitBtnClk}>CHECKOUT</button>
     </>
     )
 }
